4dab427f0b3c9af7dfe4ccd1928a9b2791e65088
Ben Marks <onodric@yahoo.com>
2017-01-24 13:18:47 -0600

    Added ProductCategory serializer to serializer.py

--- Bangazon/Bangazon_api/serializers.py
+++ Bangazon/Bangazon_api/serializers.py
@@ -2,11 +2,26 @@ from rest_framework import serializers
 from Bangazon_api.models import *
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Class for data serialization of a specific Model: User
+    """
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'date_of_birth')
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Class for data serialization of a specific Model: Product
+    """
     class Meta:
         model = Product
         fields = ('id', 'title', 'description', 'price', 'product_category_id', 'user_id',)
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    """
+    Class for data serialization of a specific Model: ProductCategory
+    """
+    class Meta:
+        model = ProductCategory
+        fields = ('id', 'name',)
