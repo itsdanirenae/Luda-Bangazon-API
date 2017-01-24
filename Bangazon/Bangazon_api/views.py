5bcdffc10cdb0f4ea7b103e12e864bc90ac476c9
Paul Martin <Dmart331@gmail.com>
2017-01-24 13:04:07 -0600

    Added user views to views.py

--- Bangazon/Bangazon_api/views.py
+++ Bangazon/Bangazon_api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from Bangazon_api.models import *
+from Bangazon_api.serializers import *
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def user_list(request, format=None):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
