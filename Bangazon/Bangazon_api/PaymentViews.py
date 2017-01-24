db9c9b1b532c26b15d24bc180c1cca55a472ebaa
juliakimchung <julhkm@gmail.com>
2017-01-24 15:02:19 -0600

    created paymentview

---
+++ Bangazon/Bangazon_api/PaymentViews.py
@@ -0,0 +1,16 @@
+from Bangazon_api.models import *
+from Bangazon_api.serializer import *
+from rest_framework import generics
+
+class PaymentList(generics.ListCreateAPIView):
+    """ List Product or create a new user
+    """
+    queryset = Payment.objects.all()
+    serializer_class = ProductSerializer
+
+class PaymentDetail(gnerics.RetreiveUpdateDestoryAPIView):
+    """ List selected product detail
+    """
+
+    queryset = Payment.objects.all()
+    serializer_class = ProductSerializer
