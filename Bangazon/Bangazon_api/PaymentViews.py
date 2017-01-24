dd615ee10c61b765b9e2a149111efa6e7fe88b1f
juliakimchung <julhkm@gmail.com>
2017-01-24 15:07:05 -0600

    paymentViews done

--- Bangazon/Bangazon_api/PaymentViews.py
+++ Bangazon/Bangazon_api/PaymentViews.py
@@ -6,11 +6,11 @@ class PaymentList(generics.ListCreateAPIView):
     """ List Product or create a new user
     """
     queryset = Payment.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = PaymentSerializer
 
 class PaymentDetail(gnerics.RetreiveUpdateDestoryAPIView):
     """ List selected product detail
     """
 
     queryset = Payment.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = PaymentSerializer
