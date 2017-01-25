adca08eb033d9d6f89f6b400fc3c28d176cbe7f2
Samuel Phillips <samphillips1879@gmail.com>
2017-01-25 12:07:26 -0600

    list views showing, all except ProductList

--- Bangazon/Bangazon_api/urls.py
+++ Bangazon/Bangazon_api/urls.py
@@ -3,5 +3,10 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    url(r'^users/$', UserList),
+    url(r'^users/$', UserList.as_view()),
+    url(r'^payment-methods/$', PaymentMethodList.as_view()),
+    url(r'^orders/$', OrderList.as_view()),
+    url(r'^product-categories/$', ProductCategoryList.as_view()),
+    url(r'^products/$', ProductList.as_view()),
+    url(r'^product-order/$', ProductOrderList.as_view()),
 ]
