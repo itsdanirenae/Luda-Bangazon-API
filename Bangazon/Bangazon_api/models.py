03aac68fa554a13d93cccd3caf9cf61fc10402ce
Ben Marks <onodric@yahoo.com>
2017-01-30 19:07:44 -0600

    Removed '_id' from field names in models. Fixed formatting

--- Bangazon/Bangazon_api/models.py
+++ Bangazon/Bangazon_api/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-
 class PaymentMethod(models.Model):
     """
     Class to create a table representing a Payment Method, tied to a
@@ -14,19 +12,23 @@ class PaymentMethod(models.Model):
         created: the current local date and time of creation
         name: the payment method's name
         account_number: the payment method's unique identifier
-        user_id: the foreign key of the user, related_name is for the PaymentMethod model: related_name should be lowercase, pluralized model name
+        customer: the foreign key of the user, related_name is for the
+            PaymentMethod model: related_name should be lowercase,
+            pluralized model name
 
     Authors: Julia Kim-Chung, Jack Pinkston, Sam Phillips, Drew Martin,
-    Ben Marks
+        Ben Marks
     """
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50, default='')
     account_number = models.CharField(max_length=100, blank=True, default='')
-    user_id = models.ForeignKey(User, related_name="payment_methods", on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, related_name="payment_methods",
+        on_delete=models.CASCADE)
 
     def __str__(self):
         """
-        Method to create a string representing a Payment Method of a particular User(customer) of Bangazon API
+        Method to create a string representing a Payment Method of a
+            particular User(customer) of Bangazon API
 
         Returns a string representaion of the payment method's name field
         """
@@ -35,52 +37,51 @@ class PaymentMethod(models.Model):
     class Meta:
         """
         Class defining metadata for results of querying the Payment Method
-        table of Bangazon API
+            table of Bangazon API
         """
         ordering = ('name',)
-
 
 
 class Order(models.Model):
     """
     Class to create a table representing an Order, tied to a
-    particular User(customer) of Bangazon API
+        particular User(customer) of Bangazon API
     Extension of models.Model
 
     Variables:
         active: A boolean denoting whether the order is being processed
         created: the current local date and time of creation
-        payment_method_id: the foreign key of the user's payment method, only
-            needed when the order is "active", related_name is for the Order model: related_name should be lowercase, pluralized model name
+        payment_method: the foreign key of the user's payment method, only
+            needed when the order is "active", related_name is for the Order
+            model; related_name should be lowercase, pluralized model name
 
-        user_id: the foreign key of the User(customer)related_name is for the Order model: related_name should be lowercase, pluralized model name
-
+        customer: the foreign key of the User(customer)related_name is for
+        the Order model; related_name should be lowercase, pluralized model
+        name
 
     Authors: Julia Kim-Chung, Jack Pinkston, Sam Phillips, Drew Martin,
-    Ben Marks
-    """
-    """
-    Class to represent an order on Bangazon
+        Ben Marks
     """
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    payment_method_id = models.ForeignKey("PaymentMethod", related_name="orders", on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, related_name="orders", on_delete=models.CASCADE)
+    payment_method = models.ForeignKey("PaymentMethod",
+        related_name="orders", on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, related_name="orders",
+        on_delete=models.CASCADE)
 
     def __str__(self):
         """
         Method to create a string representing a Order of a particular User
-        (customer) of Bangazon API
+            (customer) of Bangazon API
         """
         return str(self.id)
 
     class Meta:
         """
         Class defining metadata for results of querying the Order table of
-        Bangazon API
+            Bangazon API
         """
-        ordering = ('user_id',)
-
+        ordering = ('customer',)
 
 
 class ProductCategory(models.Model):
@@ -92,30 +93,29 @@ class ProductCategory(models.Model):
         name: the Product Category's name
 
     Authors: Julia Kim-Chung, Jack Pinkston, Sam Phillips, Drew Martin,
-    Ben Marks
+        Ben Marks
     """
     name = models.CharField(max_length=50, default='')
 
     def __str__(self):
         """
         Method to create a string representing a Product Category of
-        Bangazon API
+            Bangazon API
         """
         return self.name
 
     class Meta:
         """
         Class defining metadata for results of querying the Product Category
-        table of Bangazon API
+            table of Bangazon API
         """
         ordering = ('name',)
-
 
 
 class Product(models.Model):
     """
     Class to create a table representing a Product, tied to a
-    particular User(seller) of Bangazon API
+        particular User(seller) of Bangazon API
     Extension of models.Model
 
     Variables:
@@ -124,59 +124,68 @@ class Product(models.Model):
         price: the price of the product, in US dollars
         quantity_available: the amount available for purchase
         description: a description of the product
-        product_category_id: the foreign key of the product category, related_name is for the Product model: related_name should be lowercase, pluralized model name
-
-        user_id: the foreign key of the User(seller), related_name is for the Product model: related_name should be lowercase, pluralized model name
+        product_category: the foreign key of the product category,
+            related_name is for the Product model; related_name should
+            be lowercase, pluralized model name
+        seller: the foreign key of the User(seller), related_name is for
+            the Product model; related_name should be lowercase, pluralized
+            model name
 
 
     Authors: Julia Kim-Chung, Jack Pinkston, Sam Phillips, Drew Martin,
-    Ben Marks
+        Ben Marks
     """
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, default='')
     price = models.DecimalField(max_digits=20, decimal_places=2)
     quantity_available = models.IntegerField()
     description = models.TextField(max_length=300, default='')
-    product_category_id = models.ForeignKey("ProductCategory", related_name="products", on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    product_category = models.ForeignKey("ProductCategory",
+        related_name="products", on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, related_name="products",
+        on_delete=models.CASCADE)
 
     def __str__(self):
         """
         Method to create a string representing a Product sold by a
-        particular User(seller) of Bangazon API
+            particular User(seller) of Bangazon API
         """
         return self.name
 
     class Meta:
         """
         Class defining metadata for results of querying the Product table of
-        Bangazon API
+            Bangazon API
         """
         ordering = ('name',)
-
 
 
 class ProductOrder(models.Model):
     """
     Class to create a table representing a Product Order, tying products to a
-    particular Order
+        particular Order
     Extension of models.Model
 
     Variables:
-        product_id: foreign key of an added product, related_name is for the ProductOrder model: related_name should be lowercase, pluralized model name
+        product: foreign key of an added product, related_name is for the
+            ProductOrder model; related_name should be lowercase, pluralized
+            model name
 
-        order_id: foreign key of the order, related_name is for the ProductOrder model: related_name should be lowercase, pluralized model name
-
+        order: foreign key of the order, related_name is for the
+            ProductOrder model; related_name should be lowercase, pluralized
+            model name
 
     Authors: Julia Kim-Chung, Jack Pinkston, Sam Phillips, Drew Martin,
     Ben Marks
     """
-    product_id = models.ForeignKey("Product", related_name="product_orders", on_delete=models.CASCADE)
-    order_id = models.ForeignKey("Order", related_name="product_orders", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", related_name="product_orders",
+        on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", related_name="product_orders",
+        on_delete=models.CASCADE)
 
     class Meta:
         """
         Class defining metadata for results of querying the Product/Order
-        table of Bangazon API
+            table of Bangazon API
         """
-        ordering = ('order_id',)
+        ordering = ('order',)
